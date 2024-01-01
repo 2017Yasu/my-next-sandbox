1ab257fcdd1a65ea44f510e291e12a5d40d3d6cc
2017Yasu <yasu.nkym.96@gmail.com>
2024-01-01 22:44:23 +0900

    yjsを利用してTodoリストを実装

--- src/components/todo-list/todo-list.tsx
+++ src/components/todo-list/todo-list.tsx
@@ -1,34 +1,71 @@
 "use client";
 
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import * as Y from "yjs";
+import { WebrtcProvider } from "y-webrtc";
 import TodoItem from "./todo-item";
 
+type TodoItemType = {
+  id: string;
+  done: boolean;
+  description: string;
+};
+
 export default function TodoList() {
-  const [todoList, setTodoList] = useState([
-    { id: "todo1", done: false, description: "todo 1" },
-    { id: "todo2", done: true, description: "todo 2" },
-    { id: "todo3", done: false, description: "todo 3" },
-  ]);
+  const initialized = useRef(false);
+  const yDoc = useRef<Y.Doc | null>(null);
+  const yArray = useRef<Y.Array<TodoItemType> | null>(null);
+  const webrtc = useRef<WebrtcProvider | null>(null);
+
+  const [todoList, setTodoList] = useState<Array<TodoItemType>>([]);
+
+  const onYArrayChange = useCallback(
+    (e: Y.YArrayEvent<TodoItemType>, tx: Y.Transaction) => {
+      console.log("yArray changed");
+      setTodoList(e.target.toArray());
+    },
+    []
+  );
+
+  useEffect(() => {
+    console.log("initializing");
+    if (initialized.current) {
+      console.log("it has already initialized");
+      return;
+    }
+    initialized.current = true;
+    yDoc.current = new Y.Doc();
+    webrtc.current = new WebrtcProvider("todo-list-room", yDoc.current);
+    yArray.current = yDoc.current.getArray<TodoItemType>("todo list");
+    console.log("initialized");
+  }, []);
+
+  useEffect(() => {
+    yArray.current?.observe(onYArrayChange);
+    return () => {
+      yArray.current?.unobserve(onYArrayChange);
+    };
+  }, [onYArrayChange]);
 
   const onDoneStateChange = useCallback((index: number, done: boolean) => {
-    setTodoList((x) => {
-      const target = { ...x[index], done };
-      return x
-        .slice(0, index)
-        .concat([target])
-        .concat(x.slice(index + 1));
-    });
+    // setTodoList((x) => {
+    //   const target: TodoItemType = { ...x[index], done };
+    //   return x
+    //     .slice(0, index)
+    //     .concat([target])
+    //     .concat(x.slice(index + 1));
+    // });
   }, []);
 
   const onDescriptionChange = useCallback(
     (index: number, description: string) => {
-      setTodoList((x) => {
-        const target = { ...x[index], description };
-        return x
-          .slice(0, index)
-          .concat([target])
-          .concat(x.slice(index + 1));
-      });
+      // setTodoList((x) => {
+      //   const target: TodoItemType = { ...x[index], description };
+      //   return x
+      //     .slice(0, index)
+      //     .concat([target])
+      //     .concat(x.slice(index + 1));
+      // });
     },
     []
   );
@@ -43,6 +80,21 @@ export default function TodoList() {
           onDescriptionChange={(v) => onDescriptionChange(i, v)}
         />
       ))}
+      <div>
+        <button
+          onClick={() => {
+            yArray.current?.push([
+              {
+                id: Math.random().toString(),
+                done: false,
+                description: Math.random().toString(),
+              },
+            ]);
+          }}
+        >
+          Add
+        </button>
+      </div>
     </div>
   );
 }
